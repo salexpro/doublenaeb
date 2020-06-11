4ba5c48b3e208f53fcbb909f00f23e8ee69a2b6d
Alexander <dev@salex.pro>
2020-06-11 22:47:57 +0300

    add lang toggler

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ import svg4everybody from 'svg4everybody';
 // import './lib/foundation-explicit-pieces';
 
 svg4everybody();
+
+document.querySelectorAll('.lang a').forEach(el => {
+    el.addEventListener('click', e => {
+        localStorage.setItem('lang', e.target.innerText.toLowerCase())
+    })
+})
